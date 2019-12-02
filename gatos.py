85e5185e1cfbe88a5514769c117e6532f25f9319
urielmendozag <ab17100222@gmail.com>
2019-12-01 23:49:02 -0600

    gatos ejercicio añadido

---
+++ gatos.py
@@ -0,0 +1,16 @@
+# Cuantos gatos tienes?
+
+gatos = int(input())
+
+if gatos == 0:
+    print("Consiguete algunos gatos")
+elif gatos == 1:
+    print("Decente")
+elif gatos == 2:
+    print("Empiezas a preocupar")
+elif gatos > 3:
+    print("Loca por los gatos")
+else:
+    print("Acaso debes gatos? xD")
+
+
