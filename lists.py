3653e80517f893880f5769eba8d2d948f9f8d795
urielmendozag <ab17100222@gmail.com>
2019-12-08 22:18:04 -0600

    lists ejercicio añadido

---
+++ lists.py
@@ -0,0 +1,27 @@
+# Lists
+
+numbers = []
+strings = []
+names = ["Jhon","Eric","Jessica"]
+
+# Add the numbers 1,2,3 to the numbers list
+
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+# Add the words hello and world to the strings list
+
+strings.append("hello")
+strings.append("world")
+
+# Define variable second name to second name of the list
+
+second_name = names[1]
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
+
+
