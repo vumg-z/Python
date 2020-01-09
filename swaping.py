68fbcc0b2eefae42bcf57e8d0c65088de2e70170
vumg <ab17100222@gmail.com>
2020-01-09 11:29:49 -0600

    swaping numbers

---
+++ swaping.py
@@ -0,0 +1,11 @@
+# Hacer un swap sin una variable auxiliar
+
+x = 5
+y = 2 
+
+x = x + y 
+y = x - y 
+x = x - y 
+
+print("el valor de x despues del swap es: ", x)
+print("el valor de y despues del swap es: ", y)
