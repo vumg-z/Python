8fd36118b335509f43ce32922dd50ad0d32899eb
vumg <ab17100222@gmail.com>
2020-02-25 19:34:52 -0600

    diccionarios revisited

--- diccionarios.py
+++ diccionarios.py
@@ -1,30 +1,49 @@
 # Diccionarios
 
-phonebook = {
-    "mich" : 3310249922,
-    "jake" : 3318263823,
-    "rudy" : 3321223323,
-    "Jill" : 947662781
+phone_book = {
+    "uriel" : 3310249921,
+    "jhon" : 3310231231
 }
 
-# Add "Jake" to the phonebook with the phone number 938273443, and remove Jill from the phonebook.
+print("Un programa simple para añadir numeros de telefono")
+print("[1] Añadir un contacto")
+print("[2] Eliminar un contacto")
+print("[3] Leer un contacto")
+print("[4] Actualizar un contacto")
 
-phonebook["Jake"] = 9382734343
-phonebook.pop("Jill")
+option = input()
 
-# testing code
-if "Jake" in phonebook:
-    print("Jake is listed in the phonebook.")
-if "Jill" not in phonebook:
-    print("Jill is not listed in the phonebook.")
+if(option == "1"):
+    print("añadir nombre")
+    nombre = input()
+    print("añadir numero")
+    numero = input()
+    phone_book[nombre] = numero
+elif(option == "2"):
+    print("escriba el nombre del contacto a eliminar")
+    nombre = input()
+    phone_book.pop(nombre)
+elif(option == "3"):
+    print("escriba el nombre del contacto a leer")
+    nombre = input()
+    if(nombre in  phone_book):
+        print(phone_book[nombre])
+    else:
+        print("nombre no encontrado")
+elif(option == "4"):
+    print("Escriba el nombre del usuario a actualizar")
+    nombre = input()
+    if(nombre in phone_book):
+        print("Ecriba el numero nuevo")
+        numero = input()
+        phone_book[nombre] = numero
+    else:
+        print("Usuario no encontrado")
     
-# Iterating over dictionaries
 
-for name, number in phonebook.items():
-    print("Phone number of %s is %d" % (name,number))
-    
-# Removing a value
+print(phone_book)
 
-del phonebook["mich"]
 
-phonebook.pop("rudy")
+
+
+
