a33532885023f45da8849ee79e67fce9b9c961bd
vumg <ab17100222@gmail.com>
2020-03-06 12:05:43 -0600

    swapi

--- swapi.py
+++ swapi.py
@@ -1,29 +1,50 @@
 #! /usr/bin/python3
 # This script will return a random character of star wars
-import requests, random, time
+import requests
+import random
+import time
+from concurrent import futures
 from colorama import Fore, init
 
 init()
 
-# time init
-
 initialTime = time.time()
 
-names = []
-
-try:
-    url = "https://swapi.co/api/people/"    
-    r = requests.get(url = url)
-    data = r.json()
-    while(data['next'] is not None):
-        r = requests.get(url = url)
+def swapi(start, end):
+    try:
+        names = []
+        url = "https://swapi.co/api/people/{}".format(start)
+        if(end != None):
+            limit = "https://swapi.co/api/people/?page={}".format(end)
+        else:
+            limit = None
+        r = requests.get(url=url)
         data = r.json()
-        for i in data['results']:
-            names.append(i['name'])
-        url =  data['next']
-except requests.exceptions.RequestException as e:
-    print(Fore.RED+" Star Wars Character not avaliable: request error")
+        while(url != limit):
+            r = requests.get(url=url)
+            data = r.json()
+            for i in data['results']:
+                names.append(i['name'])
+            url = data['next']
+    except requests.exceptions.RequestException as e:
+        print(Fore.RED+" Star Wars Character not avaliable: request error", e)
+    return names
+
+
+ex = futures.ThreadPoolExecutor(max_workers=5)
+
+wait_for = [
+    ex.submit(swapi, '', '3'),
+    ex.submit(swapi, '?page=3', '6'),
+    ex.submit(swapi, '?page=6', None)
+]
+
+characters = []
+
+for f in futures.as_completed(wait_for):
+    characters += f.result()
+
 
 finalTime = round(time.time() - initialTime, 2)
+print(Fore.BLUE+characters[random.randint(0,len(characters))],Fore.GREEN+"(in "+str(finalTime)+" seconds)")
 
-print(Fore.BLUE+names[random.randint(0,len(names))] + Fore.GREEN+" (in "+str(finalTime)+" seconds)")
