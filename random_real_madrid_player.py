fe020e699c3aab91dedfbe68afa13d23e3dc164b
vumg <ab17100222@gmail.com>
2020-03-05 20:28:14 -0600

    try catch added

--- random_real_madrid_player.py
+++ random_real_madrid_player.py
@@ -1,21 +1,22 @@
 #! /usr/bin/python3
 import http.client, json, random, time
 
-# init time
-initialTime = time.time()
+try:
+    # init time
+    initialTime = time.time()
+    connection = http.client.HTTPConnection('api.football-data.org')
+    headers = { 'X-Auth-Token': 'cf37aebe74444dc2a8c8e463b8ebbff7' }
+    connection.request('GET', '/v2/teams/86', None, headers )
+    response = json.loads(connection.getresponse().read().decode())
 
-connection = http.client.HTTPConnection('api.football-data.org')
-headers = { 'X-Auth-Token': 'cf37aebe74444dc2a8c8e463b8ebbff7' }
-connection.request('GET', '/v2/teams/86', None, headers )
-response = json.loads(connection.getresponse().read().decode())
+    names = []
 
-names = []
+    for i in response['squad']:
+        names.append(i['name'])
 
-for i in response['squad']:
-    names.append(i['name'])
+    finalTime = round(time.time() - initialTime, 2)
+    print(names[random.randint(0,len(names))], "in", str(finalTime), "seconds")
 
-print(names[random.randint(0,len(names))])
+except Exception as e:
+    print("real madrid player not available: request error")
 
-finalTime = round(time.time() - initialTime, 2)
-
-print(str(finalTime)+" seconds")
