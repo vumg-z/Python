011c4c1b2a7fec9cc400e874586b41b4e5a82786
vumg <ab17100222@gmail.com>
2020-03-06 12:06:12 -0600

    swapi

--- threads.py
+++ threads.py
@@ -6,9 +6,13 @@ def poke():
 def real():
     subprocess.call('python random_real_madrid_player.py', shell=True)
 
+def swapi():
+    subprocess.call('python swapi.py', shell=True)
+
 threadPoke = threading.Thread(target=poke)
 threadReal = threading.Thread(target=real)
+threadSwapi = threading.Thread(target=swapi)
 
 threadPoke.start()
 threadReal.start()
-
+threadSwapi.start()
