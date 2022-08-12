5abd25ecb8b0960e365cc11056c0c90a44c31a00
um <ab17100222@gmail.com>
2022-08-12 16:28:27 -0500

    Sphere Exercise

--- math_ex.py
+++ math_ex.py
@@ -11,4 +11,9 @@ number_of_books = 60
 book_printing_price = (initial_price * discount) * 60
 shipping_total = (shipping_cost * 1) + .75 * (number_of_books - 1)
 
-print(book_printing_price + shipping_total) # 945.44 
+print(book_printing_price + shipping_total) # 945.44
+
+# The volume of a sphere with radius r is 4/3 π r3. What is the volume of a sphere with radius 5?
+
+volume = 4/3*3.14*5**3
+print(volume)
